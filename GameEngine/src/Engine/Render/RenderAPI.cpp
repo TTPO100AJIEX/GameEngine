a782234c50994ddd6ae40cb92bfb4fe260664c52
Александр Абрамов <ttpo100ajiex@mail.ru>
2022-04-09 19:11:25 +0300

    Minor RenderAPI change

---
+++ GameEngine/src/Engine/Render/RenderAPI.cpp
@@ -0,0 +1,76 @@
+#include <PrecompiledHeaders.h>
+
+#include "RenderAPI.h"
+
+namespace GameEngine::RenderAPI
+{
+	#ifdef RENDER_USE_OPENGL
+		std::shared_ptr<::GameEngine::Renderer> Create()
+		{
+			return(std::make_shared<::GameEngine::OpenGLRenderer>());
+		}
+	#endif
+
+	namespace Camera
+	{
+		std::shared_ptr<::GameEngine::Render::Camera> Create(float left, float right, float bottom, float top)
+		{
+			return(std::make_shared<::GameEngine::Render::Camera>(left, right, bottom, top));
+		}
+	}
+
+	#ifdef RENDER_USE_OPENGL
+
+		namespace VertexBuffer
+		{
+			namespace Layout
+			{
+				std::shared_ptr<::GameEngine::Render::VertexBufferLayout> Create(const std::initializer_list<::GameEngine::Render::VertexBufferLayoutElement>& Elements)
+				{
+					return(std::make_shared<::GameEngine::Render::OpenGLLayout>(Elements));
+				}
+			}
+			std::shared_ptr<::GameEngine::Render::VertexBuffer> Create(void* Data, size_t Amount, const std::shared_ptr<::GameEngine::Render::VertexBufferLayout>& Layout)
+			{
+				return(std::make_shared<::GameEngine::Render::OpenGLVertexBuffer>(Data, Amount, Layout));
+			}
+		}
+		namespace IndexBuffer
+		{
+			std::shared_ptr<::GameEngine::Render::IndexBuffer> Create(uint32_t* Data, size_t Amount)
+			{
+				return(std::make_shared<::GameEngine::Render::OpenGLIndexBuffer>(Data, Amount));
+			}
+		}
+		namespace VertexArray
+		{
+			std::shared_ptr<::GameEngine::Render::VertexArray> Create()
+			{
+				return(std::make_shared<::GameEngine::Render::OpenGLVertexArray>());
+			}
+		}
+
+		namespace Shader
+		{
+			std::shared_ptr<::GameEngine::Render::Shader> CreateFromStrings(const std::unordered_map<::GameEngine::Render::ShaderType, const std::string>& data)
+			{
+				return(std::make_shared<Render::OpenGLShader>(data, false));
+			}
+			std::shared_ptr<::GameEngine::Render::Shader> CreateFromFiles(const std::unordered_map<::GameEngine::Render::ShaderType, const std::string>& data)
+			{
+				return(std::make_shared<::GameEngine::Render::OpenGLShader>(data, true));
+			}
+		}
+
+		namespace Texture
+		{
+			std::shared_ptr<::GameEngine::Render::Texture2D> Create2D(const std::string& path)
+			{
+				return(std::make_shared<::GameEngine::Render::OpenGLTexture2D>(path));
+			}
+		}
+
+	#else
+		#error "One of [RENDER_USE_OPENGL] must be defined"
+	#endif
+}
--- GameEngine/src/Engine/Render/RenderAPI.h
+++ GameEngine/src/Engine/Render/RenderAPI.h
@@ -13,76 +13,38 @@
 
 namespace GameEngine::RenderAPI
 {
+	std::shared_ptr<::GameEngine::Renderer> Create();
+
 	namespace Camera
 	{
-		inline std::shared_ptr<Render::Camera> Create(float left, float right, float bottom, float top)
-		{
-			return(std::make_shared<Render::Camera>(left, right, bottom, top));
-		}
+		std::shared_ptr<::GameEngine::Render::Camera> Create(float left, float right, float bottom, float top);
 	}
 
-	#ifdef RENDER_USE_OPENGL
-		inline std::shared_ptr<Renderer> Create()
+	namespace VertexBuffer
+	{
+		namespace Layout
 		{
-			return(std::make_shared<OpenGLRenderer>());
+			std::shared_ptr<::GameEngine::Render::VertexBufferLayout> Create(const std::initializer_list<::GameEngine::Render::VertexBufferLayoutElement>& Elements);
 		}
+		std::shared_ptr<::GameEngine::Render::VertexBuffer> Create(void* Data, size_t Amount, const std::shared_ptr<::GameEngine::Render::VertexBufferLayout>& Layout);
+	}
+	namespace IndexBuffer
+	{
+		std::shared_ptr<::GameEngine::Render::IndexBuffer> Create(uint32_t* Data, size_t Amount);
+	}
+	namespace VertexArray
+	{
+		std::shared_ptr<::GameEngine::Render::VertexArray> Create();
+	}
 
-		namespace VertexArray
-		{
-			inline std::shared_ptr<Render::VertexArray> Create()
-			{
-				return(std::make_shared<Render::OpenGLVertexArray>());
-			}
-		}
+	namespace Shader
+	{
+		std::shared_ptr<::GameEngine::Render::Shader> CreateFromStrings(const std::unordered_map<::GameEngine::Render::ShaderType, const std::string>& data);
+		std::shared_ptr<::GameEngine::Render::Shader> CreateFromFiles(const std::unordered_map<::GameEngine::Render::ShaderType, const std::string>& data);
+	}
 
-		namespace VertexBuffer
-		{
-			namespace Layout
-			{
-				inline std::shared_ptr<Render::VertexBufferLayout> Create(const std::initializer_list<Render::VertexBufferLayoutElement>& Elements)
-				{
-					return(std::make_shared<Render::OpenGLLayout>(Elements));
-				}
-			}
-			inline std::shared_ptr<Render::VertexBuffer> Create(void* Data, size_t Amount, const std::shared_ptr<Render::OpenGLLayout>& Layout)
-			{
-				return(std::make_shared<Render::OpenGLVertexBuffer>(Data, Amount, Layout));
-			}
-			inline std::shared_ptr<Render::VertexBuffer> Create(void* Data, size_t Amount, const std::shared_ptr<Render::VertexBufferLayout>& Layout)
-			{
-				return(Create(Data, Amount, std::static_pointer_cast<Render::OpenGLLayout>(Layout)));
-			}
-		}
-
-		namespace IndexBuffer
-		{
-			inline std::shared_ptr<Render::IndexBuffer> Create(uint32_t* Data, size_t Amount)
-			{
-				return(std::make_shared<Render::OpenGLIndexBuffer>(Data, Amount));
-			}
-		}
-
-		namespace Shader
-		{
-			inline std::shared_ptr<Render::Shader> CreateFromStrings(const std::unordered_map<GameEngine::Render::ShaderType, const std::string>& data)
-			{
-				return(std::make_shared<Render::OpenGLShader>(data, false));
-			}
-			inline std::shared_ptr<Render::Shader> CreateFromFiles(const std::unordered_map<GameEngine::Render::ShaderType, const std::string>& data)
-			{
-				return(std::make_shared<Render::OpenGLShader>(data, true));
-			}
-		}
-
-		namespace Texture
-		{
-			inline std::shared_ptr<Render::Texture2D> Create2D(const std::string& path)
-			{
-				return(std::make_shared<Render::OpenGLTexture2D>(path));
-			}
-		}
-
-	#else
-		#error "One of [RENDER_USE_OPENGL] must be defined"
-	#endif
+	namespace Texture
+	{
+		std::shared_ptr<::GameEngine::Render::Texture2D> Create2D(const std::string& path);
+	}
 }
